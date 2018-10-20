911cae5309e609e0c3747d9e0aacc8de2ec0ddb9
Dom Hudson <dom.hudson@thoughtriver.com>
2018-10-20 22:33:04 +0100

    Adding license and readme

--- setup.py
+++ setup.py
@@ -3,11 +3,24 @@ import os
 from distutils.core import setup
 from setuptools import find_packages
 
+def get_readme():
+    """ Get the package README as a string.
+
+    :return str
+    """
+    root = os.path.abspath(os.path.dirname(__file__))
+    readme_path = os.path.join(root, 'README.md')
+
+    with open(readme_path) as f:
+        return f.read()
 
 setup(
     name = 'lmdb_embeddings',
-    version = '0.2.0',
+    version = '0.2.1',
     description = 'Fast querying of word embeddings using the LMDB "Lightning" Database.',
+    license = 'GNU General Public License v3.0',
+    long_description = get_readme(),
+    long_description_content_type = 'text/markdown',
     author = 'Dom Hudson',
     author_email = 'dom.hudson@thoughtriver.com',
     url = 'https://www.thoughtriver.com',
